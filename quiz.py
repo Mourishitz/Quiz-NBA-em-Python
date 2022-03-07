f29e96531afa4646a5fbd7ce04c30704ca3e73df
Gabriel Morishita <97261428+Mourishitz@users.noreply.github.com>
2022-03-06 23:50:40 -0300

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,3 +1,4 @@
+# função para iniciar o jogo + declaração de variaveis
 def novo_jogo():
     escolha_usuario = []
     escolha_correta = 0
@@ -7,17 +8,18 @@ def novo_jogo():
         print(i)
         for j in opcoes[numero_questao-1]:
             print(j)
+        # algoritmo para a escolha de respostas e ganho de pontos
         escolha = input("Resposta?: ")
         escolha = escolha.upper()
-        escolha_usuario.append(escolha)
-
+        escolha_usuario.append(escolha) 
+        
         escolha_correta += resposta(questoes.get(i),escolha)
         numero_questao += 1
 
     pontos(escolha_correta, escolha)
 
 
-
+# algoritmo para averiguar a resposta certa e calcular o total de pontos
 def resposta(resposta_certa, escolha):
 
     if resposta_certa == escolha:
@@ -26,7 +28,7 @@ def resposta(resposta_certa, escolha):
     else:
         print("Resposta Errada :(")
         return 0
-
+# algoritmo de resultado final com base nas escolhas do usuario
 def pontos(escolha_correta, escolha):
     print("---------------------------------------------------------------------")
     print("Resultados")
@@ -39,6 +41,8 @@ def pontos(escolha_correta, escolha):
 
     score = int((escolha_correta/len(questoes))*100)
     print("Sua pontuação é de: "+str(score)+"%")
+    
+# algoritmo para reiniciar o jogo
 def jogar_dnv():
 
     response = input("Você deseja jogar de novo? (sim/nao): ")
@@ -48,20 +52,20 @@ def jogar_dnv():
         return True
     else:
         return False
-#----------
+# Questões do jogo em seguida de sua resposta
 questoes = {
-        "Qual foi o primeiro time a marcar 72-10 em uma temporada de Basquete?: ": "A",
+        "Qual foi o primeiro time a marcar 72-10 em uma temporada regular de Basquete?: ": "A",
         "Qual o jogador com mais titulos de campeão da NBA até 2021?: ": "B",
-        "Quem é o maior cestinha de todos os tempos da NBA?: ": "C",
+        "Pela primeira vez na história um time conseguiu vencer uma final tendo uma desvantagem de 3 derrotas e 1 vitória, qual time é este?: ": "C",
         "Quais jogadores faziam parte do famoso 'Big 3' do Miami Heat em 2010?: ": "A"
     }
-
+# Respostas 
 opcoes = [
         ["A. Chicago Bulls", "B. Los Angeles Lakers", "C. Boston Celtics", "D. Miami Heat"],
         ["A. Michael Jordan", "B. Bill Russel", "C. Shaquille O'Neal", "D. Lebron James"],
-        ["A. Kobe Bryant", "B. Karl Malone", "C. Kareem Abdul-Jabbar", "D. Steph Curry"],
-        ["A. Lebron James, Dwyane Wade, Chris Bosh", "B. Shaquille O'Neal, Kobe Bryant, Lamar Odom",
-         "C. Kyrie Irving, Kevin Durant, James Harden", "D. Anthony Davis, Lebron James, Russel Westbrook"]
+        ["A. Oklahoma City Thunder", "B. Phoenix Suns", "C. Cleveland Cavaliers", "D. Golden State Warriors"],
+        ["A. Lebron James, Dwyane Wade, Chris Bosh", "B. Jimmy Butler, Bam Adebayo, Tyler Herro",
+         "C. Dwayne Wade, Shaquille O'Neal, Gary Payton", "D. Anthony Davis, Lebron James, Russel Westbrook"]
     ]
 novo_jogo()
 
